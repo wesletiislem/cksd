603d0f864caa07fa4c3fcaf42299b5e26334b005
wesletiislem <esleem250@gmail.com>
2022-02-16 12:14:20 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./Components/Navbar.js";
+import Main from './Components/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+         <Header/>
+         <Main/>
+     </div> 
+   
+  )
+  
+}
+
+export default App;
